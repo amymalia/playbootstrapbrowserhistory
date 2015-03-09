993edf62f6d841af40587d6670292b44313864e4
amymalia <amytakayesu@gmail.com>
2015-03-09 03:01:18 -1000

    Added multipage functionality
    Added multi pages using the play framework

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,9 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Index;
+import views.html.ie;
+import views.html.ff;
+import views.html.chrome;
 
 /**
  * Provides controllers for this application.
@@ -15,6 +18,30 @@ public class Application extends Controller {
    */
   public static Result index() {
     return ok(Index.render("Welcome to the home page."));
+  }
+
+  /**
+   * Returns the ie page.
+   * @return The resulting ie page.
+   */
+  public static Result ie() {
+    return ok(ie.render("Welcome to the home page."));
+  }
+
+  /**
+   * Returns the ff page.
+   * @return The resulting ff page.
+   */
+  public static Result ff() {
+    return ok(ff.render("Welcome to the home page."));
+  }
+
+  /**
+   * Returns the chrome page.
+   * @return The resulting chrome page.
+   */
+  public static Result chrome() {
+    return ok(chrome.render("Welcome to the home page."));
   }
 
 
--- test/test/ApplicationTest.java
+++ test/test/ApplicationTest.java
@@ -33,5 +33,35 @@ public class ApplicationTest {
     assertThat(contentAsString(html)).contains("History");
   }
 
+  /**
+   * Tests that the EE template renders correctly.
+   */
+  @Test
+  public void renderEE() {
+    Content html = views.html.Index.render("Welcome to the home page.");
+    assertThat(contentType(html)).isEqualTo("text/html");
+    assertThat(contentAsString(html)).contains("Internet Explorer");
+  }
+
+  /**
+   * Tests that the FF template renders correctly.
+   */
+  @Test
+  public void renderFF() {
+    Content html = views.html.Index.render("Welcome to the home page.");
+    assertThat(contentType(html)).isEqualTo("text/html");
+    assertThat(contentAsString(html)).contains("Firefox");
+  }
+
+  /**
+   * Tests that the Chrome template renders correctly.
+   */
+  @Test
+  public void renderChrome() {
+    Content html = views.html.Index.render("Welcome to the home page.");
+    assertThat(contentType(html)).isEqualTo("text/html");
+    assertThat(contentAsString(html)).contains("Chrome");
+  }
+
 
 }
